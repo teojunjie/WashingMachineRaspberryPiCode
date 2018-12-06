e004e9294b83fee7b8884fa9366c443d5d212e85
teojunjie <e0148239@u.nus.edu>
2018-12-06 22:49:37 +0800

    Added lcd code to color sensor code

--- ColorSensorCode1.py
+++ ColorSensorCode1.py
@@ -1,6 +1,17 @@
+#############################
+#Washing Machine Sensor Code#
+#############################
+
 import RPi.GPIO as GPIO
 import time
 import requests
+import os
+import socket
+import fcntl
+import struct
+import time
+from time import gmtime, strftime
+
 
 # Defining GPIO pins configuration
 s2 = 20
@@ -12,6 +23,27 @@ greenPin = 19
 bluePin = 26
 PIRPin = 4
 VIBPin = 15
+
+## Define GPIO to LCD mapping
+LCD_RS = 7
+LCD_E  = 8
+LCD_D4 = 25
+LCD_D5 = 24
+LCD_D6 = 23
+LCD_D7 = 18
+
+# Define some device constants
+LCD_WIDTH = 16    # Maximum characters per line
+LCD_CHR = True
+LCD_CMD = False
+
+LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
+LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line
+
+# Timing constants
+E_PULSE = 0.0005
+E_DELAY = 0.0005
+
 
 # Defining the api-endpoint 
 API_ENDPOINT = "https://washingmachineserverke7.herokuapp.com/machineStatus"
@@ -42,6 +74,21 @@ def setup():
   GPIO.setup(bluePin, GPIO.OUT)
   GPIO.setup(PIRPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
   GPIO.setup(VIBPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+  # Setting up GPIO pins for LCD
+  GPIO.setwarnings(False)
+  GPIO.setmode(GPIO.BCM)       # Use BCM GPIO numbers
+  GPIO.setup(LCD_E, GPIO.OUT)  # E
+  GPIO.setup(LCD_RS, GPIO.OUT) # RS
+  GPIO.setup(LCD_D4, GPIO.OUT) # DB4
+  GPIO.setup(LCD_D5, GPIO.OUT) # DB5
+  GPIO.setup(LCD_D6, GPIO.OUT) # DB6
+  GPIO.setup(LCD_D7, GPIO.OUT) # DB7
+
+  print("Initializing display")
+  # Initialise display
+  lcd_init()
+
 
 def detectRed() :
   GPIO.output(s2,GPIO.LOW)
@@ -154,6 +201,8 @@ def loop():
 
   while(1):
     color = getCorrectColor()
+    lcd_string("Color detected :",LCD_LINE_1)
+    lcd_string(color,LCD_LINE_2)
     sendPostRequest(color)
     lightLED(color)
     detectMovement()
@@ -167,7 +216,7 @@ def sendPostRequest(color):
 
     print("Washing machine available")
     print("Sending ON post request")
-
+    
     response = requests.post(url = API_ENDPOINT, data = dataON) 
 
   else :
@@ -180,7 +229,77 @@ def sendPostRequest(color):
   message = response.text 
   print("The response message is: %s"%message)    
 
+def lcd_init():
+  # Initialise display
+  lcd_byte(0x33,LCD_CMD) # 110011 Initialise
+  lcd_byte(0x32,LCD_CMD) # 110010 Initialise
+  lcd_byte(0x06,LCD_CMD) # 000110 Cursor move direction
+  lcd_byte(0x0C,LCD_CMD) # 001100 Display On,Cursor Off, Blink Off
+  lcd_byte(0x28,LCD_CMD) # 101000 Data length, number of lines, font size
+  lcd_byte(0x01,LCD_CMD) # 000001 Clear display
+  time.sleep(E_DELAY)
 
+def lcd_toggle_enable():
+  # Toggle enable
+  time.sleep(E_DELAY)
+  GPIO.output(LCD_E, True)
+  time.sleep(E_PULSE)
+  GPIO.output(LCD_E, False)
+  time.sleep(E_DELAY)
+
+def lcd_byte(bits, mode):
+  # Send byte to data pins
+  # bits = data
+  # mode = True  for character
+  #        False for command
+
+  GPIO.output(LCD_RS, mode) # RS
+
+  # High bits
+  GPIO.output(LCD_D4, False)
+  GPIO.output(LCD_D5, False)
+  GPIO.output(LCD_D6, False)
+  GPIO.output(LCD_D7, False)
+  if bits&0x10==0x10:
+    GPIO.output(LCD_D4, True)
+  if bits&0x20==0x20:
+    GPIO.output(LCD_D5, True)
+  if bits&0x40==0x40:
+    GPIO.output(LCD_D6, True)
+  if bits&0x80==0x80:
+    GPIO.output(LCD_D7, True)
+
+  # Toggle 'Enable' pin
+  lcd_toggle_enable()
+
+  # Low bits
+  GPIO.output(LCD_D4, False)
+  GPIO.output(LCD_D5, False)
+  GPIO.output(LCD_D6, False)
+  GPIO.output(LCD_D7, False)
+  if bits&0x01==0x01:
+    GPIO.output(LCD_D4, True)
+  if bits&0x02==0x02:
+    GPIO.output(LCD_D5, True)
+  if bits&0x04==0x04:
+    GPIO.output(LCD_D6, True)
+  if bits&0x08==0x08:
+    GPIO.output(LCD_D7, True)
+
+  # Toggle 'Enable' pin
+  lcd_toggle_enable()
+
+
+def lcd_string(message,line):
+  # Cast to string
+  message = str(message)
+  # Send string to display
+  message = message.ljust(LCD_WIDTH," ")
+
+  lcd_byte(line, LCD_CMD)
+
+  for i in range(LCD_WIDTH):
+    lcd_byte(ord(message[i]),LCD_CHR)
   
 def endprogram():
     GPIO.cleanup()
